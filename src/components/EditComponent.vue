1f742d62c30d9799c0716f54f53bbd967e674a61
Aranza Cabrera <33462552+alu0100889680@users.noreply.github.com>
2019-05-25 20:51:22 +0100

    Cuarto

--- src/components/EditComponent.vue
+++ src/components/EditComponent.vue
@@ -34,15 +34,19 @@ export default {
     };
   },
   created() {
-    let uri = `http://localhost:4000/posts/edit/${this.$route.params.id}`;
+    // let uri = `http://localhost:4000/posts/edit/${this.$route.params.id}`;
+    let uri =
+      `http://localhost:` +
+      process.env.PORT +
+      `/posts/edit/${this.$route.params.id}`;
     this.axios.get(uri).then(response => {
       this.post = response.data;
     });
   },
   methods: {
     updatePost() {
-      let uri = `http://localhost:4000/posts/update/${this.$route.params.id}`;
-      `http://localhost:` +
+      let uri =
+        `http://localhost:` +
         process.env.PORT +
         `/posts/update/${this.$route.params.id}`;
       this.axios.post(uri, this.post).then(() => {
