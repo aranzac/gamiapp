706e0c53778b1fbaeab4c0ac8d4ceca0192264c4
Aranza Cabrera <33462552+alu0100889680@users.noreply.github.com>
2019-08-13 23:53:23 +0100

    Prueba ssl #3

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ const juegosRoute = require('./routes/juegos.route');
 
 /////////// SSL //////////
 const sslRedirect = require('heroku-ssl-redirect');
-const secure = require('express-force-https');
+// const secure = require('express-force-https');
 
 
 
@@ -59,7 +59,7 @@ app.use('/juegos', juegosRoute);
 ///////// SSL MIDDLEWARE /////////
 app.use(sslRedirect);
 
-app.use(secure)
+// app.use(secure)
 
 
 app.use((req, res, next) => {
